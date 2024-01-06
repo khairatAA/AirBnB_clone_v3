4e6278af8ead3097e55022257555aac88107e207
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-06 22:29:23 +0100

    add: checked status of API

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+"""index module"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def get_status():
+    """get_status: gets the status """
+    status_check = {"status": "OK"}
+    return jsonify(status_check)
