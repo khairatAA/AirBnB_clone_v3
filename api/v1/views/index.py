3c2100ddf826e009f7e9ec026182700de21d8de5
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-07 04:02:35 +0100

    fix: route and pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,13 +17,14 @@ def get_status():
     status_check = {"status": "OK"}
     return jsonify(status_check)
 
+
 @app_views.route('/stats')
 def retrieve_object_number():
     """Retrieves the number of each objects by type"""
     object_list = [Amenity, City, Place, Review, State, User]
     name_list = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
 
-    object_dict ={}
+    object_dict = {}
     for index, object in enumerate(object_list):
         object_count = storage.count(object)
         object_dict[name_list[index]] = object_count
