ba3da79c41677247746da5ea749a10395f9187d0
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-07 14:01:04 +0100

    add: added a out for page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """app module"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -8,6 +8,12 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    error_content = {"error": "Not found"}
+    return jsonify(error_content), 404
 
 
 @app.teardown_appcontext
